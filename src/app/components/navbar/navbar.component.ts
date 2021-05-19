002a44070b4732c5dc536d50db88bdff6f2db87a
GiovanniDBL <Donitho@hotmail.com>
2021-05-19 00:48:57 -0500

    version1.0.1

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -10,6 +10,25 @@ export class NavbarComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+  header_variable = false;
+  
+  @HostListener('document:scroll')
+  scrollfunction(){
+    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+      this.header_variable = true;
+    }
+    else{
+      this.header_variable = false;
+    }
+  }
+
+  
+  toInicio(){
+    document.getElementById('home')?.scrollIntoView({behavior: 'smooth'});
+  }
+  toAboutUs(){
+    document.getElementById('aboutus')?.scrollIntoView({behavior: 'smooth'});
   }
 
 }
